8f64d504276dbc7156f23c0c703fe9819c4cf4f4
Matt Dobson <mdobson4@gmail.com>
2013-11-28 11:53:35 -0500

    Updated to use new coker lib.

--- index.js
+++ index.js
@@ -4,30 +4,34 @@ var cmdr = require('commander');
 cmdr
   .version('0.0.0')
   .option('--host [host]', 'Couchdb host')
+  .option('--dockerport [host]', 'Docker host')
+  .option('--app [app]', 'Deployed app name')
   .option('-c --container [id]', 'container id')
-  .option('-p --port [port]', 'Open port on host machine')
-  .option('-a --app [app]', 'App name')
-  .option('-n --name [name]', 'Container name')
-  .option('-h --hash [hash]', 'git md5 hash')
   .parse(process.argv);
 
 var db = require('nano')(cmdr.host);
+var docker = require('docker.io')({ socketPath:false, host: 'http://localhost', port: cmdr.dockerport.toString()});
 
-if (!cmdr.container && !cmdr.port && !cmdr.app && !cmdr.name) {
+if(!cmdr.container) {
   console.log("Error recording deployment! Routing will be affected.");
 } else {
-  var deploy = {
-    containerId : cmdr.container,
-    port : cmdr.port,
-    app : cmdr.app,
-    name : cmdr.name,
-    hash : cmdr.hash
-  };
-  db.insert(deploy, deploy.name, function(err, body) {
-    if(err) {
-      console.log("Deploy error. Routing may not work!");
-    } else {
-      console.log("Router updated.");
-    }
+  docker.containers.inspect( cmdr.container, function(err, res) {
+    var name = res.Name;
+    var portConfigs = Object.keys(res.NetworkSettings.Ports);
+    var hostPort = res.NetworkSettings.Ports[portConfigs][0].HostPort;
+    var deploy = {
+      containerId : cmdr.container,
+      port : hostPort,
+      app : cmdr.app,
+      name : name,
+      hash : cmdr.hash
+    };
+    db.insert(deploy, deploy.name, function(err, body) {
+      if(err) {
+        console.log("Deploy error. Routing may not work!");
+      } else {
+        console.log("Router updated.");
+      }
+    });
   });
 }
